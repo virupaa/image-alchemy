1dd80e4b3118832ff5060c99b1c5d34bd0f9de9a
Pathak, Chinmay Rajendra <crpathak@iu.edu>
2024-05-29 11:31:19 -0400

    webhook-final-implementation

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,11 @@
-import { authMiddleware, clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-import { NextResponse } from "next/server";
 
-// export default authMiddleware({
-//   publicRoutes:['/api/webhooks/clerk']
-// });
-
-export const config = {
-  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-};
-
-const isProtectedRoute = createRouteMatcher([
-    "/",
-    // "/api(.*)"
-  ]);
-
-export default clerkMiddleware((auth, request) => {
-  publicRoutes:['/api/webhooks/clerk']
-  
-  if(isProtectedRoute(request)){
-      auth().protect();
-  }
+import { authMiddleware } from "@clerk/nextjs";
+ 
+export default authMiddleware({
+  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
 });
+ 
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
