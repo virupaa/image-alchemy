3bcf3b42506a3688bdb45be77faf85fa915891c4
Pathak, Chinmay Rajendra <crpathak@iu.edu>
2024-05-29 18:45:29 -0400

    webhook-final-implementation

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
-  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
+  publicRoutes: ['/', '/api/webhooks/clerk']
 });
  
 export const config = {
